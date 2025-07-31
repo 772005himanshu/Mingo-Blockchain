f5da898060a29bff3e20f3935c2ce3a7d97f314e
Himanshu <23bme029@iiitdmj.ac.in>
2025-08-01 03:18:56 +0530

    Address

--- core/transaction.go
+++ core/transaction.go
@@ -4,7 +4,6 @@ import "io"
 
 type Transaction struct {
 	Data []byte
-	
 }
 
 
---
+++ crypto/keypair.go
@@ -0,0 +1,48 @@
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"github.com/772005himanshu/Mingo-Blockchain/types"
+);
+
+type PrivateKey struct {
+	key *ecdsa.PrivateKey
+}
+
+func GeneratePrivateKey() PrivateKey {
+	key , err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader) // Never Use the Rand due to determinitic Nature of the Blockchain
+	if err != nil {
+		panic(err)
+	}
+
+	return PrivateKey{
+		key: key,
+	}
+}
+
+func (k PrivateKey) PublicKey() PublicKey {
+	return PublicKey {
+		key : &k.key.PublicKey,
+	}
+}
+
+type PublicKey struct {
+	key *ecdsa.PublicKey
+}
+
+func (k PublicKey) ToSlice() []byte {
+	return elliptic.MarshalCompressed(k.key, k.key.X, k.key.Y)
+}
+
+func (k PublicKey) Address() types.Address {
+	h := sha256.Sum256(k.ToSlice())
+
+	return types.AddressFromBytes(h[len(h) - 20:]);
+}
+
+type Signature struct {
+
+}
